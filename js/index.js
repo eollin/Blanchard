fe41b5af4f8ced0fc46e33a49e6d1c331f6a07c0
eollin <eydos@list.ru>
2022-06-23 12:16:02 +0300

    gallery pagination added

--- js/index.js
+++ js/index.js
@@ -28,13 +28,14 @@ const swiper2 = new Swiper('.swiper-container-2', {
   spaceBetween: 50,
   loop: false,
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-button-next-2',
+    prevEl: '.swiper-button-prev-2',
     hideOnClick: 'false'
   },
 
   pagination: {
-    el: '.swiper-pagination',
+    el: '.swiper-pagination-2',
+    type: 'fraction',
     clickable: true,
   },
 
